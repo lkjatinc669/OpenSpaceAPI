8e33a245e202d7d1b9c121ebaecfd2058190658e
Jatin Gohil <gohiljatin660@gmail.com>
2023-07-28 22:57:42 +0530

    Some Changes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// use port 3000 unless there exists a preconfigured port
-var port = process.env.PORT || 3000;
 
 app.listen(port, function (err) {
     if(err){
